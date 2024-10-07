60c0fc85fdc23237b70c570474e28c677bc67f86
egor-shishkarev <egorsiskarev92@gmail.com>
2024-10-07 17:37:19 +0300

    Add finding interpolation nodes using the Chebyshev method, constructing the Lagrange interpolation polynomial

---
+++ AlgebraicInterpolation/main.py
@@ -0,0 +1,59 @@
+# f(x) = sqrt(1+x^2), xj = a + j * (b - a)/m, j = 0...m, a = 0, b = 0.7, x = 0.4, n = 7, m = 15.
+
+from math import sqrt
+from utils import int_check, float_check, print_table
+from methods import (
+    create_preparatory_table, 
+    get_value_of_Lagrange_polynomial,
+    get_inaccuracy,
+    sort_table_according_to_point)
+
+function_string = "√(1+x^2)"
+
+def function(x: float):
+    return sqrt(1 + x ** 2)
+
+print('Задача алгебраического интерполирования. Интерполяционный многочлен в форме Лагранжа.')
+print(f'Номер варианта - 4, функция - {function_string}')
+count_of_points = int_check('Введите число m - количество точек в таблице => ')
+left_border = float_check('Введите левую границу отрезка => ')
+right_border = float_check('Введите правую границу отрезка => ')
+while (left_border >= right_border):
+    print('Левая граница не может быть больше правой! Повторите ввод.')
+    left_border = float_check('Введите левую границу отрезка => ')
+    right_border = float_check('Введите правую границу отрезка => ')
+
+preparatory_table = create_preparatory_table(function, count_of_points, left_border, right_border)
+
+print_table(preparatory_table)
+
+while (True):
+    desired_point = float_check('Введите точку, в которой хотите найти приближенное значение функции => ')
+
+    while (True):
+        degree_of_interpolation_polynomial = int_check('Введите степень интерполяционного многочлена => ')
+        if (degree_of_interpolation_polynomial + 1 > count_of_points):
+            print('Степень интерполяционного многочлена не может быть больше, чем количество точек - 1!')
+            print('Повторите ввод.')
+            continue
+        break
+
+    sorted_table = sort_table_according_to_point(preparatory_table, desired_point, degree_of_interpolation_polynomial)
+
+    print(f'Исходные данные -\nФункция - {function_string},\nОтрезок - [{left_border}, {right_border}],',
+          f'\nСтепень искомого многочлена - {degree_of_interpolation_polynomial},\nТаблица значений -')
+
+    print_table(sorted_table)
+
+    desired_value = get_value_of_Lagrange_polynomial(desired_point, sorted_table)
+
+    print(f'Значение в точке {desired_point} вычисленное с помощью интерполяционного многочлена Лагранжа - {desired_value}')
+
+    inaccuracy = get_inaccuracy(desired_value, desired_point, function)
+
+    print(f'Фактическая погрешность значения - {inaccuracy}')
+
+    decision = input('Хотите ввести новые значения искомой точки и степень многочлена? (Y/N)')
+    
+    if decision != 'Y':
+        break
---
+++ AlgebraicInterpolation/methods.py
@@ -0,0 +1,49 @@
+from typing import Callable, List
+from math import cos, pi
+
+def create_preparatory_table(function: Callable[[float], float], count_of_points: int, left_border: float, right_border: float):
+    """
+
+    """
+    nodes: List[List[float]] = []
+
+    # Use Chebyshev nodes
+    chebyshev_nodes: List[float] = []
+
+    for i in range(count_of_points + 1):
+        chebyshev_nodes.append(cos(pi * (2 * i + 1) / (2 * count_of_points + 1)))
+
+    for i in range(count_of_points + 1):
+        current_point = (left_border + right_border) / 2 + (right_border - left_border) / 2 * chebyshev_nodes[i]
+        nodes.append([current_point, function(current_point)])
+
+    return nodes
+
+def get_value_of_Lagrange_polynomial(desired_point: float, sorted_table: List[List[float]]):
+    """
+
+    """
+    desired_value = 0
+
+    for i in range(len(sorted_table)):
+        numerator = 0
+        denominator = 0
+        for j in range(len(sorted_table)):
+            if (i != j):
+                numerator += (desired_point - sorted_table[j][0])
+                denominator += (sorted_table[i][0] - sorted_table[j][0])
+        desired_value += (numerator / denominator) * sorted_table[i][1]
+        
+    return desired_value
+
+def get_inaccuracy(desired_value: float, desired_point: float, function: Callable[[float], float]):
+    """
+
+    """
+    return abs(desired_value - function(desired_point))
+
+def sort_table_according_to_point(table: List[List[float]], point: float, degree_of_interpolation_polynomial: int):
+    """
+    
+    """
+    return sorted(table, key=lambda element: abs(element[0] - point))[:degree_of_interpolation_polynomial + 1]
