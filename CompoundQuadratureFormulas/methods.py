3b9a9b881965b437774e170d5c38db6ee867871f
egor-shishkarev <egorsiskarev92@gmail.com>
2024-11-03 19:25:30 +0300

    fix: fix simpson method and type of property "count_of_intervals"
    feat: add checking algebraic degree of accuracy

--- CompoundQuadratureFormulas/methods.py
+++ CompoundQuadratureFormulas/methods.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, List
 
 def left_rectangle(
         function: Callable[[float], float],
         down_border: float,
         up_border: float,
-        count_of_intervals: float
+        count_of_intervals: int
     ):
     step = (up_border - down_border) / count_of_intervals
     value = 0
@@ -16,7 +16,7 @@ def right_rectangle(
         function: Callable[[float], float],
         down_border: float,
         up_border: float,
-        count_of_intervals: float
+        count_of_intervals: int
     ):
     step = (up_border - down_border) / count_of_intervals
     value = 0
@@ -28,7 +28,7 @@ def middle_rectangle(
         function: Callable[[float], float],
         down_border: float,
         up_border: float,
-        count_of_intervals: float
+        count_of_intervals: int
     ):
     step = (up_border - down_border) / count_of_intervals
     value = 0
@@ -40,7 +40,7 @@ def trapezoid(
         function: Callable[[float], float],
         down_border: float,
         up_border: float,
-        count_of_intervals: float
+        count_of_intervals: int
     ):
     step = (up_border - down_border) / count_of_intervals
     value = 0
@@ -57,30 +57,31 @@ def simpson(
         function: Callable[[float], float],
         down_border: float,
         up_border: float,
-        count_of_intervals: float
-    ):
+        count_of_intervals: int
+    ):   
     step = (up_border - down_border) / count_of_intervals
     value = 0
-    for i in range(count_of_intervals + 1):
-        if (i == 0):
-            value += function(down_border)
-        elif (i == count_of_intervals):
-            value += function(up_border)
-        else:
-            value += 2 * function(down_border + i * step)
-            value += 4 * function(down_border + (i + 1 / 2) * step)
-    return step / 6 * value
+    
+    def point_j(j: float):
+        return down_border + j * step
+
+    for j in range(0, count_of_intervals):
+        value += function(point_j(j))
+        value += 4 * function(point_j(j + 1 / 2))
+        value += function(point_j(j + 1))
+    
+    return (step / 6) * value
 
 def print_table(
         function: Callable[[float], float],
         down_border: float,
         up_border: float,
-        count_of_intervals: float,
+        count_of_intervals: int,
         accurate_integral: float
     ):
 
-    header_format = "{:<23} {:<23} {:<24} {:<27}"
-    row_format = "{:<23} {:<23.10e} {:<24.10e} {:<27.10f}"
+    header_format = "{:>23} {:>23} {:>24} {:>27}"
+    row_format = "{:>23} {:>23.10e} {:>24.10e} {:>26.10f}%"
     print()
     print(header_format.format("Метод СКФ", "Приближенное значение", "Абсолютная погрешность", "Относительная погрешность"))
     print('-' * 100)
@@ -91,14 +92,14 @@ def print_table(
         approximate_value = methods_functions[i](function, down_border, up_border, count_of_intervals)
         inaccuracy = abs(approximate_value - accurate_integral)
         relative_inaccuracy = inaccuracy / abs(accurate_integral)
-        print(row_format.format(methods_names[i], approximate_value, inaccuracy, relative_inaccuracy))
+        print(row_format.format(methods_names[i], approximate_value, inaccuracy, relative_inaccuracy * 100))
 
 def print_runge_correction(
         function: Callable[[float], float],
         down_border: float,
         up_border: float,
-        count_of_intervals: float,
-        multiplier: float,
+        count_of_intervals: int,
+        multiplier: int,
         accurate_integral: float
     ):
     values_m = []
@@ -113,15 +114,44 @@ def print_runge_correction(
         values_m.append(value_m)
         values_ml.append(value_ml)
 
-    header_format = "{:<23} {:<23} {:<24} {:<27}"
-    row_format = "{:<23} {:<23.10e} {:<24.10e} {:<27.10f}"
+    header_format = "{:>23} {:>23} {:>24} {:>27}"
+    row_format = "{:>23} {:>23.10e} {:>24.10e} {:>26.10f}%"
     print('\nУточнение по принципу Рунге:')
     print(header_format.format("Метод СКФ", "Приближенное значение", "Абсолютная погрешность", "Относительная погрешность"))
     print('-' * 100)
     for i in range(5):
         runge_value = runge_correction(values_m[i], values_ml[i], multiplier, ADA[i])
         inaccuracy = abs(runge_value - accurate_integral)
-        print(row_format.format(methods_names[i], runge_value, inaccuracy, inaccuracy / abs(accurate_integral)))
+        print(row_format.format(methods_names[i], runge_value, inaccuracy, inaccuracy / abs(accurate_integral) * 100))
     
 def runge_correction(first_value: float, second_values: float, multiplier: float, ADA: float):
     return (multiplier ** (ADA + 1) * second_values - first_value) / (multiplier ** (ADA + 1) - 1)
+
+
+def check_ADA(
+        down_border: float,
+        up_border: float,
+        polynomials: List[Callable[[float], float]],
+        antiderivatives: List[Callable[[float], float]],
+        count_of_intervals: int
+    ):
+
+    print("Метод вычисления: ")
+    methods_names = ['Левый прямоугольник', 'Правый прямоугольник', 'Средний прямоугольник', 'Трапеция', 'Симпсон', '3/8']
+    methods_functions = [left_rectangle, right_rectangle, middle_rectangle, trapezoid, simpson]
+    ADA = [0, 0, 1, 1, 3]
+    for i in range(5):
+        print(f'\n{i + 1})', methods_names[i], f'АСТ = {ADA[i]}:')
+        def sub_function(function: Callable[[float], float]):
+            return methods_functions[i](function, down_border, up_border, count_of_intervals)
+        
+        header_format = "\t{:>11} {:>23} {:>17} {:>17}"
+        row_format = "\t{:>11} {:>23.10e} {:>17.10e} {:>17.10e}"
+        print(header_format.format("Многочлен", "Приближенное значение", "Точное значение", "Погрешность"))
+        print("\t" + "-" * 71)
+        for j in range(ADA[i] + 1):
+            approximate_value = sub_function(polynomials[j])
+            accurate_integral = abs(antiderivatives[j](up_border) - antiderivatives[j](down_border))
+            inaccuracy = abs(approximate_value - accurate_integral)
+            print(row_format.format("1" * (j == 0) + "x" * (j == 1) + f"x^{j}" * (j >= 2), approximate_value, accurate_integral, inaccuracy))
+            
