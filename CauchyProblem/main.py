365823e66e1d096add921ebc496313173d8ced1f
egor-shishkarev <egorsiskarev92@gmail.com>
2024-12-16 14:58:37 +0300

    feat: add comparison table

--- CauchyProblem/main.py
+++ CauchyProblem/main.py
@@ -16,27 +16,41 @@ taylor_function_string = "1/2 - 1/4 * x + 1/48 * x^3 - 1/480 * x^5 + 17/80640 * 
 print('\nЧисленное решение Задачи Коши для обыкновенного дифференциального уравнения первого порядка\n')
 print(f'Дифференциальное уравнение - {function_string}')
 print('Задача Коши: y(0) = 0,5')
+print(f'Разложение в ряд Тейлора - {taylor_function_string}')
+
+last_values = []
 
 while True:
-    count_of_points = int_check('Введите количество точек (> 2) => ')
-    if count_of_points <= 2:
-        print('Неверное количество точек. Повторите ввод (> 2)')
+
+    while True:
+        count_of_points = int_check('Введите количество точек (> 2) => ')
+        if count_of_points <= 2:
+            print('Неверное количество точек. Повторите ввод (> 2)')
+            continue
+        break
+    step = float_check('Введите шаг h (расстояние между точками) => ')
+    parameters = (original_function, function, step, count_of_points, original_point)
+   
+    last_values.append(print_table(taylor_function, *parameters))
+
+    last_values.append(print_table(euler_method, *parameters))
+
+    last_values.append(print_table(euler_first_method, *parameters))
+
+    last_values.append(print_table(euler_second_method, *parameters))
+
+    last_values.append(print_table(runge_kutta_method, *parameters))
+
+    #print_adams_table(function, step, count_of_points, [taylor_function(original_point + step * i) for i in range(-2, 3)])
+
+    last_values.append(print_table(extrapolation_adams_method, *parameters))
+
+    # Для всех методов найти абсолютную погрешность для последней точки
+    # Предложить ввести новые точки
+
+    print_comparison_table(last_values, original_function, original_point + count_of_points * step)
+    last_values = []
+    decision = input('Хотите ввести новые параметры задачи? (Y/N) => ')
+    if decision.lower() == 'y':
         continue
     break
-step = float_check('Введите шаг h (расстояние между точками) => ')
-parameters = (original_function, function, step, count_of_points, original_point)
-
-print_table(taylor_function, *parameters)
-
-print_table(euler_method, *parameters)
-
-print_table(euler_first_method, *parameters)
-
-print_table(euler_second_method, *parameters)
-
-print_table(runge_kutta_method, *parameters)
-
-print_table(extrapolation_adams_method, *parameters)
-
-# Для всех методов найти абсолютную погрешность для последней точки
-# Предложить ввести новые точки
--- CauchyProblem/utils.py
+++ CauchyProblem/utils.py
@@ -38,9 +38,19 @@ def print_table(calc_function: Callable[[float], float], original_function: Call
         runge_kutta_method: "по Рунге-Кутта",
         extrapolation_adams_method: "по Адамсу"
     }
-    name = function_names.get(calc_function)
 
-    print(f'\nСравнение точного решения и решения "{name}"\n')
+    inaccuracy_of_functions = {
+        taylor_function: "",
+        euler_method: "- O(h^2)",
+        euler_first_method: "- O(h^3)",
+        euler_second_method: "- O(h^3)",
+        runge_kutta_method: "- O(h^5)",
+        extrapolation_adams_method: "- O(h^6)",
+    }
+    name = function_names.get(calc_function)
+    inaccuracy = inaccuracy_of_functions.get(calc_function)
+
+    print(f'\nСравнение точного решения и решения "{name}" {inaccuracy}\n')
     header_format = "{:>6} {:>15} {:>20} {:>25} {:>15}"
     row_format = "{:>6} {:>15.6f} {:>20.6e} {:>25.6e} {:>15.6e}"
     
@@ -56,10 +66,14 @@ def print_table(calc_function: Callable[[float], float], original_function: Call
     else:
         calc_function_values = calc_function(original_function(original_point), function,  step, count_of_points)
         start, end = 1, count_of_points + 1
+
+    last_value = 0
     for i in range(start, end):
         point = original_point + i * step
         function_value = original_function(point)
         calc_function_value = calc_function_values[i - start]
+        if i == end - 1:
+            last_value = calc_function_value
         print(row_format.format(
             f'x{i}',
             point,
@@ -68,6 +82,19 @@ def print_table(calc_function: Callable[[float], float], original_function: Call
             abs(function_value - calc_function_value),
         ))
     print('-' * 85)
+    return last_value
+
+def print_comparison_table(last_values: List[float], original_function: Callable[[float], float], last_point: float):
+    header_format = "{:<15} {:<20} {:<25} {:<20}"
+    row_format = "{:<15} {:<20.6e} {:<25.6e} {:<20.6e}"
+    print(f'\nСравнительная таблица для последней точки - {last_point}\n')
+    print(header_format.format('Метод', 'Точное значение', 'Приближенное значение', 'Погрешность'))
+    methods = ['Тейлор', 'Эйлер', 'Эйлер I', 'Эйлер II', 'Рунге-Кутта', 'Адамс']
+    accurate_value = original_function(last_point)
+    print('-' * 75)
+    for i in range(len(last_values)):
+        print(row_format.format(methods[i], accurate_value, last_values[i], abs(last_values[i] - accurate_value)))
+    print('-' * 75)
 
 def print_adams_table(function: Callable[[float], float], step: float, count_of_points: int, values: List[float]):
     [values, function_values, first_differences, second_differences, third_differences, fourth_differences] = extrapolation_adams_method(values, function, step, count_of_points)
